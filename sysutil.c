a2c63b484cf68ddf025ccd28b9b20f496fed6551
npowell1600 <108088533+npowell1600@users.noreply.github.com>
2022-06-23 15:11:31 -0400

    Add files via upload

---
+++ sysutil.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+   printf("*****************************\n");
+   printf("*** ACO350 - Your Name    ***\n");
+   printf("***  System Info Utility  ***\n");
+   printf("*****************************\n\n");
+
+   return 0;
+}
